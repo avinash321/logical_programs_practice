0e789a721fca60f28f9cc0a512ee6448186eab75
Avinash Kandagatla <20856529+avinash321@users.noreply.github.com>
2026-01-08 14:25:59 +0000

    Add check polindrome program

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+word = input("Enter your word: ")
+
+if word == word[::-1]:
+    print(f"Given word '{word}' is a Palindrome")
+else:
+    print(f"Given word '{word}' is Not a Palindrome")
+          
+    
+    
+
